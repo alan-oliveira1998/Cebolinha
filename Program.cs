2ebb02acffae493214a0209c44df8231c2a4c3c8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 19:46:41 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+String Cebolinha;
+// frase do Cebolinha
+Console.WriteLine("ola eu sou o cebolinha sela que voce podelia me ajuda a escleve uma flase:");
+Console.WriteLine();
+Cebolinha=Console.ReadLine()! .Replace("r","l") .ToUpper();
+Console.WriteLine("===============================");
+Console.ForegroundColor =ConsoleColor.Yellow;
+Console.WriteLine($"// {Cebolinha} //");
+Console.ResetColor();
+Console.WriteLine();
